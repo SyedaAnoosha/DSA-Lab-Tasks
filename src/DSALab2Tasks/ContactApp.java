49d160fc070cfd5cb20b441095356809410adb48
SyedaAnoosha <anushazaidi06@gmail.com>
2022-12-11 23:53:31 +0500

    changes

--- src/DSALab2Tasks/ContactApp.java
+++ src/DSALab2Tasks/ContactApp.java
@@ -1,4 +1,41 @@
 package DSALab2Tasks;
 
+import java.util.Arrays;
+
 public class ContactApp {
+    public void menu(){
+        System.out.println(
+                "1. Display all contact\n" +
+                "2. Search a contact \n" +
+                "3. Add a new contact\n" +
+                "4. Update the contact \n" +
+                "5. Delete any contact\n");
+    }
+    public void display_contacts(String[] name, String[] num){
+        System.out.print("[");
+        for (int i = 0; i < num.length-1; i++) {
+            System.out.println(name[i]+" "+num[i]+",");
+        }
+        System.out.println(name[name.length-1]+" "+num[num.length-1]+"]");
+    }
+    public void search_contacts(String name, String[] Names, String[] Numbers){
+        for (int i = 0; i < Names.length; i++) {
+            if (name.equals(Names[i])){
+                System.out.println( "Name: " + name );
+                System.out.println( "Number: " + Numbers[i] );
+                break;
+            }
+        }
+    }
+    public void update_contacts(String name1, String name2, String[] Names){
+        System.out.println();
+        for (int i = 0; i < Names.length; i++) {
+            if (name1.equals(Names[i])){
+                Names[i]=name2;
+            }
+        }
+        System.out.println(Arrays.toString(Names));
+    }
+
+
 }
--- src/DSALab2Tasks/Main.java
+++ src/DSALab2Tasks/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] numArr={2,3,4,5};
+       /* int[] numArr={2,3,4,5};
         Lab2 a = new Lab2();
         a.arrInput();
         a.arrSearch(numArr);
@@ -36,6 +36,31 @@ public class Main {
                 }
             }
         }
+*/
 
+        String[] contacts = {"Aaron","Berkley","Charlotte","Dylan","Ella"};
+        String[] number = {"0968965672","223615371","031213","1231232134","321312315"};
+
+        ContactApp c= new ContactApp();
+
+        c.menu();
+        System.out.println("Enter the option: ");
+        int i = sc.nextInt();
+        if (i == 1) {
+            c.display_contacts(contacts, number);
+        } else if (i == 2) {
+            System.out.println("Enter the name you want to search:  ");
+            String name = sc.next();
+            c.search_contacts(name,contacts,number);
+        } else if (i == 3) {
+            System.out.print("Enter name you want to edit: ");
+            String name1= sc.nextLine();
+            System.out.print("Enter name you want to replace with it: ");
+            String name2 = sc.nextLine();
+            c.update_contacts(name1,name2,contacts);
+        }
+
+
+        c.update_contacts("Aaron","Alex",contacts);
     }
 }
