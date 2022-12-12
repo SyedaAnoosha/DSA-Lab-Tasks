682df4eee0788ebc92624319cad878061b0ed713
SyedaAnoosha <anushazaidi06@gmail.com>
2022-12-13 00:01:43 +0500

    changes

--- src/DSALab2Tasks/ContactApp.java
+++ src/DSALab2Tasks/ContactApp.java
@@ -4,12 +4,7 @@ import java.util.Arrays;
 
 public class ContactApp {
     public void menu(){
-        System.out.println(
-                "1. Display all contact\n" +
-                "2. Search a contact \n" +
-                "3. Add a new contact\n" +
-                "4. Update the contact \n" +
-                "5. Delete any contact\n");
+        System.out.println("1. Display all contact\n2. Search a contact \n3. Add a new contact\n4. Update the contact \n5. Delete any contact\n6. Exit");
     }
     public void display_contacts(String[] name, String[] num){
         System.out.print("[");
@@ -17,6 +12,31 @@ public class ContactApp {
             System.out.println(name[i]+" "+num[i]+",");
         }
         System.out.println(name[name.length-1]+" "+num[num.length-1]+"]");
+    }
+    public void add_contact(int pos, String name, String num, String[] Names, String[] Numbers){
+
+        String[] newName = new String[Names.length + 1] ;
+        String[] newNum = new String[Numbers.length + 1];
+
+        for (int i = 0; i < Names.length; i++) {
+            if(pos==i){
+                newName[i] = name;
+            }
+            else{
+                newName[i]=Names[i-1];
+            }
+        }
+        System.out.println(Arrays.deepToString(newName));
+
+        for (int i = 0; i < Numbers.length; i++) {
+            if(pos==i){
+                newNum[i] = num;
+            }
+            else{
+                newNum[i]=Numbers[i-1];
+            }
+        }
+        System.out.println(Arrays.deepToString(newNum));
     }
     public void search_contacts(String name, String[] Names, String[] Numbers){
         for (int i = 0; i < Names.length; i++) {
@@ -27,11 +47,11 @@ public class ContactApp {
             }
         }
     }
-    public void update_contacts(String name1, String name2, String[] Names){
+    public void update_contacts(String replace_element, String new_element, String[] Names){
         System.out.println();
         for (int i = 0; i < Names.length; i++) {
-            if (name1.equals(Names[i])){
-                Names[i]=name2;
+            if (replace_element.equals(Names[i])){
+                Names[i]=new_element;
             }
         }
         System.out.println(Arrays.toString(Names));
--- src/DSALab2Tasks/Main.java
+++ src/DSALab2Tasks/Main.java
@@ -39,28 +39,42 @@ public class Main {
 */
 
         String[] contacts = {"Aaron","Berkley","Charlotte","Dylan","Ella"};
-        String[] number = {"0968965672","223615371","031213","1231232134","321312315"};
+        String[] number = {"0968965672","223615371","031987213","1231232134","321312315"};
+        int i ;
 
         ContactApp c= new ContactApp();
 
         c.menu();
+
         System.out.println("Enter the option: ");
-        int i = sc.nextInt();
-        if (i == 1) {
-            c.display_contacts(contacts, number);
-        } else if (i == 2) {
-            System.out.println("Enter the name you want to search:  ");
-            String name = sc.next();
-            c.search_contacts(name,contacts,number);
-        } else if (i == 3) {
-            System.out.print("Enter name you want to edit: ");
-            String name1= sc.nextLine();
-            System.out.print("Enter name you want to replace with it: ");
-            String name2 = sc.nextLine();
-            c.update_contacts(name1,name2,contacts);
+
+        while(true){
+            i = sc.nextInt();
+            if (i == 1) {
+                c.display_contacts(contacts, number);
+            } else if (i == 2) {
+                System.out.println("Enter the name you want to search:  ");
+                String name = sc.next();
+                c.search_contacts(name,contacts,number);
+            } else if (i == 3) {
+                System.out.print("Enter at which position you want to add: ");
+                int pos =sc.nextInt();
+                System.out.println("Enter name: ");
+                String name = sc.nextLine();
+                System.out.println("Enter number");
+                String num = sc.next();
+                c.add_contact(pos,name,num,contacts,number);
+            } else if (i == 4) {
+                System.out.print("Enter name you want to replace: ");
+                String name1= sc.nextLine();
+                System.out.print("Enter name you want to replace with it: ");
+                String name2 = sc.nextLine();
+                c.update_contacts(name1,name2,contacts);
+            } else if (i == 5) {
+                System.out.println(i);
+            } else if (i == 6) {
+                System.exit(0);
+            }
         }
-
-
-        c.update_contacts("Aaron","Alex",contacts);
     }
 }
