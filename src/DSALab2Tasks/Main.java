3126b7769846faaa95b577db90405871be683f42
SyedaAnoosha <anushazaidi06@gmail.com>
2022-12-13 23:52:18 +0500

    changes

--- src/DSALab2Tasks/Main.java
+++ src/DSALab2Tasks/Main.java
@@ -5,39 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       /* int[] numArr={2,3,4,5};
-        Lab2 a = new Lab2();
-        a.arrInput();
-        a.arrSearch(numArr);
-        a.arrOutput(numArr);
-
-        if(a.arrSearch(numArr)){
-            System.out.println("Found");
-        }
-        else{
-            System.out.println("Not found");
-        }
-
-        a.menu();
-        System.out.print("Enter the option: ");
-        int op = sc.nextInt();
-        while(op!=4){
-            switch (op) {
-                case 1 -> {
-                    a.arrInput();
-                    break;
-                }
-                case 2 -> {
-                    a.arrOutput(numArr);
-                    break;
-                }
-                case 3 -> {
-                    a.arrSearch(numArr);
-                }
-            }
-        }
-*/
-
         int i;
         ContactApp c = new ContactApp();
 
@@ -73,5 +40,40 @@ public class Main {
                 System.exit(0);
             }
         }
+
+
+        /* int[] numArr={2,3,4,5};
+        Lab2 a = new Lab2();
+        a.arrInput();
+        a.arrSearch(numArr);
+        a.arrOutput(numArr);
+
+        if(a.arrSearch(numArr)){
+            System.out.println("Found");
+        }
+        else{
+            System.out.println("Not found");
+        }
+
+        a.menu();
+        System.out.print("Enter the option: ");
+        int op = sc.nextInt();
+        while(op!=4){
+            switch (op) {
+                case 1 -> {
+                    a.arrInput();
+                    break;
+                }
+                case 2 -> {
+                    a.arrOutput(numArr);
+                    break;
+                }
+                case 3 -> {
+                    a.arrSearch(numArr);
+                }
+            }
+        }
+*/
+
     }
 }
